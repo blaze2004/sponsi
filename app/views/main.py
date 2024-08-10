84be777dce5f0dc327f635677c7d77c43853ea3b
Shubham Tiwari <shubhamtiwari06112004@gmail.com>
2024-08-10 23:16:12 +0530

    added users info on admin dashboard

--- app/views/main.py
+++ app/views/main.py
@@ -1,6 +1,9 @@
 """Marketing Pages views."""
 
 from flask import Blueprint, render_template
+from flask_login import current_user, login_required
+from app.models.user import User, UserRole
+from app.utils import db
 
 main = Blueprint("main", __name__)
 
@@ -10,7 +13,41 @@ def index():
     """Render the landing page."""
     return render_template("index.html")
 
+
 @main.route("/dashboard")
+@login_required
 def dashboard():
     """Render the dashboard according to user role."""
+    if current_user.role in (UserRole.ADMIN, UserRole.SUPERADMIN):
+        users = db.session.execute(db.select(User).order_by(User.id)).scalars()
+        users_data = []
+
+        for user in users:
+            users_data.append(
+                {
+                    "id": user.id,
+                    "name": user.name,
+                    "email": user.email,
+                    "role": user.role.name,
+                    "about": user.about,
+                    "flagged": user.flagged,
+                    "flagged_reason": user.flagged_reason,
+                    "onboarded": user.onboarded,
+                    "website": user.website,
+                    "company_name": user.company_name,
+                    "industry": user.industry,
+                    "platforms": [
+                        {
+                            "platform": platform.platform,
+                            "username": platform.username,
+                            "url": platform.url,
+                            "followers": platform.followers,
+                        }
+                        for platform in user.platforms
+                    ],
+                    "niche": user.niche,
+                    "audience_size": user.audience_size,
+                }
+            )
+        return render_template("dashboard/admin.html", users=users_data)
     return render_template("dashboard.html")
